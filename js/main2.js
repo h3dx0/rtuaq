4f8dd0904fdf607358bcf6c8546ad72d87ae4681
Roberto Nuñez <rcorralesn@gmail.com>
2015-02-09 13:38:30 -0600

    bug maps show

--- js/main2.js
+++ js/main2.js
@@ -1,15 +1,20 @@
 $(document).ready(function(){
-	
+	$('#imgContainer').hide();
 	$('select#selRutas').on('change',function(){
 		var name = $(this).val();	
-		console.log(name);
+		$('#imgContainer').show();
 		$('#mapaImg').attr('src','img/'+name+'.png');
 	});
 
 
 $('select#selCamion').on('change',function(){
 		var name = $(this).val();	
+		$('#imgContainer').show();
 		$('#mapaImg').attr('src','img/'+name+'.png');
 	});
-
+$('#horario_btn').on('click',function(){
+		
+		$('#imgContainer').hide();
+		
+	});
 })
