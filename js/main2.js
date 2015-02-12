b873ef0df519b90b9f04d0b3f475552fe36d2149
Roberto Nuñez <rcorralesn@gmail.com>
2015-02-12 12:49:24 -0600

    cambios UI

--- js/main2.js
+++ js/main2.js
@@ -1,19 +1,39 @@
 $(document).ready(function(){
 	$('#imgContainer').hide();
+	$('#horario').hide();
 	$('select#selRutas').on('change',function(){
-		var name = $(this).val();	
-		$('#imgContainer').show();
-		$('#mapaImg').attr('src','img/'+name+'.png');
-	});
-
-
-$('select#selCamion').on('change',function(){
-		var name = $(this).val();	
-		$('#imgContainer').show();
-		$('#mapaImg').attr('src','img/'+name+'.png');
-	});
-$('#horario_btn').on('click',function(){
 		
+		$('#horario').hide();
+		var name = $(this).val();
+		var texto = $("select#selRutas option:selected" ).text();	
+		if (name!='inicio') {
+			$('#msj').html('Ruta del camión de '+ texto);
+		} else{
+			$('#msj').html('');
+		};
+		$('#imgContainer').show();
+		$('#mapaImg').attr('src','img/'+name+'.png');
+		$('select#selRutas option[value=inicio]').prop('selected','selected');
+	});
+
+
+	$('select#selCamion').on('change',function(){
+		
+		$('#horario').hide();		
+		var name = $(this).val();	
+		var texto = $("select#selCamion option:selected" ).text();
+		if (name!='inicio') {
+			$('#msj').html('Posición del camión de '+ texto);
+		} else{
+			$('#msj').html('');
+		};
+		
+		$('#imgContainer').show();
+		$('#mapaImg').attr('src','img/'+name+'.png');
+		$('select#selCamion option[value=inicio]').prop('selected','selected');
+	});
+	$('#horario_btn').on('click',function(){
+		$('#horario').show();		
 		$('#imgContainer').hide();
 		
 	});
